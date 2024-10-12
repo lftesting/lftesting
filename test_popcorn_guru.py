3c74cfb2442b4d878e21d2482717e990f4a5aa70
Lucas Ferrari <lucasferrari89@gmail.com>
2024-10-12 20:47:40 +0200

    Create test_popcorn_guru.py
    Unit testing PopcornGuru project

---
+++ test_popcorn_guru.py
@@ -0,0 +1,16 @@
+# unit testing PopcornGuru project - IN PROCESS
+
+from project import fetch_movies, collect_preferences, create_gui
+import pytest
+
+def test_fetch_movies():
+    ...
+
+
+def test_collect_preferences():
+    ...
+
+
+def test_create_gui():
+    ...
+
