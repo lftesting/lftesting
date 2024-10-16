74501edb804f41905d5d24ccb8dddb078fa49741
Lucas Ferrari <lucasferrari89@gmail.com>
2024-10-16 15:54:13 +0200

    Create products.py

---
+++ products.py
@@ -0,0 +1,10 @@
+products = {
+    "001": {"name": "Apple", "price":0.30, "stock": 100},
+    "002": {"name": "Banana", "price": 0.50, "stock": 50},
+    "003": {"name" : "Milk", "price": 1.00, "stock": 30},
+    "004": {"name": "Lemon", "price": 0.40, "stock": 70},
+    "005": {"name": "Avocado", "price": 1.50, "stock": 25},
+    "006": {"name": "Detergent", "price": 1.25, "stock": 30},
+    "007": {"name": "Tomato", "price": 0.50, "stock": 50},
+    "008": {"name": "Rice", "price": 1, "stock": 25} 
+}
