5322bb7bffa16313ae349fee63d9a43a5f7fe9f4
Lucas Ferrari <lucasferrari89@gmail.com>
2024-10-17 19:49:08 +0200

    Update PyPOS.py

--- PyPOS.py
+++ PyPOS.py
@@ -1,3 +1,6 @@
+### THIS PROJECT IS FULLY DEVELOPED BUT CODE IS NOT PUBLISHED BECAUSE OF ACADEMIC HONESTY POLICIES. 
+### AVAILABLE UPON REQUEST FOR COLLABORATION OR NON ACADEMIC INTERESTS
+
 from products import products
 
 sales = []
@@ -28,65 +31,4 @@ def main():
         else:
             print("Invalid choice, please try again.")
 
-
-def sales_report():
-    print("\nSales Report:")
-    print("Total sales:", len(sales))
-    total_revenue = sum(sale["total"] for sale in sales)
-    print(f"Total revenue: ${total_revenue:.2f}")
-
-def process_sale():
-    cart = []
-    total = 0
-    while True:
-        add = input("Enter Product ID or 'done' to finish: ").lower().strip()
-        if add == "done":
-            break
-        if add in products:
-            quantity = int(input("Enter quantity: "))
-            if quantity <= products[add]["stock"]:
-                price = products[add]["price"] * quantity
-                total += price
-                cart.append((add, quantity, price))
-                print(f"Added {quantity} {products[add]["name"]} to your cart. Total: ${total:.2f}.")
-            else:
-                print("Not enough stock.")
-        else:
-            print("Invalid product. Please try again.")
-
-    print("\nFinalizing Sale:")
-    print("Product  | Quantity | Price")
-    print("---------------------------")
-    for item in cart:
-        print(f"{products[item[0]]['name']} | {item[1]} | ${item[2]:.2f}")
-        products[item[0]]['stock'] -= item[1]  
-
-    print(f"\nTotal amount due: ${total:.2f}")
-    sales.append({"cart": cart, "total": total})
-    print("Sale completed!\n")
-
-
-def add_product():
-    product_id = input("Enter Product ID: ")
-    name = input("Enter Product Name: ")
-    price = float(input("Enter Product Price: "))
-    stock = int(input("Enter Stock Quantity: "))
-    
-    products[product_id] = {"name": name, "price": price, "stock": stock}
-    print(f"Product {name} added successfully!")
-
-def display_products():
-    print("\nAvailable Products:")
-    print("ID  | Name   | Price | Stock")
-    print("-----------------------------")
-    for product_id, details in products.items():
-        print(f"{product_id} | {details['name']} | ${details['price']:.2f} | {details['stock']}")
-
-
-def inventory_report():
-    print("\nInventory Report:")
-    display_products()
-
-
-if __name__ == "__main__":
-    main()
+#### REST OF THE CODE AVAILABLE UPON REQUEST. FULLY DEVELOPED PROJECT.
